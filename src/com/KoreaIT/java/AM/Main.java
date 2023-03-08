b102939c39f808087142ff9f98defe0236746fad
lifenell0411 <lifenell0411@gmail.com>
2023-03-08 17:42:30 +0900

    게시물 작성 기능 추가

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -8,13 +8,37 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
+		int lastId = 1;
 
 		while (true) {
 			System.out.print("명령어 > ");
 			String command = sc.nextLine();
-			
-			if(command.equals("exit")) {
+
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (command.equals("exit")) {
 				break;
+			}
+			if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			}
+
+			else if (command.equals("article write")) {
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				System.out.println(lastId + "번글이 생성되었습니다.");
+				lastId++;
+
+			}
+
+			else {
+				System.out.println("존재하지 않는 명령어입니다.");
+
 			}
 		}
 
