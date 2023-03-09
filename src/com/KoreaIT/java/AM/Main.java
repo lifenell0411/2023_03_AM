10e3dac89e7a4ad88a6717c61b4cd24780ecae5b
lifenell0411 <lifenell0411@gmail.com>
2023-03-09 15:43:24 +0900

    게시물 상세보기 및 삭제기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.AM;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,10 +14,12 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int lastId = 0;
 		List<Article> articles = new ArrayList<>();
+		LocalDateTime now = LocalDateTime.now();
+		String formatNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		while (true) {
 
 			System.out.print("명령어 > ");
-			String command = sc.nextLine();
+			String command = sc.nextLine().trim();
 
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
@@ -30,7 +34,7 @@ public class Main {
 					System.out.println("게시글이 없습니다.");
 				}
 
-				else if (articles.size() != 0) {
+				else {
 					for (int i = articles.size() - 1; i >= 0; i--) {
 						Article article = articles.get(i);
 						System.out.println("번호" + "  //  " + "제목");
@@ -38,6 +42,51 @@ public class Main {
 					}
 				}
 
+			}
+
+			else if (command.startsWith("article detail ")) {
+				String[] cmdDiv = command.split(" ");
+				int id = Integer.parseInt(cmdDiv[2]);
+				boolean found = false;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						found = true;
+						System.out.println("번호 : " + article.id);
+						System.out.println("날짜:" + formatNow);
+						System.out.println("제목:" + article.title);
+						System.out.println("내용:" + article.body);
+						break;
+
+					}
+				}
+				if (found == false) {
+					System.out.println(id + "번 게시물은 존재하지 않습니다.");
+				}
+
+			}
+
+			else if (command.startsWith("article delete ")) {
+				String[] cmdDiv = command.split(" ");
+				int id = Integer.parseInt(cmdDiv[2]);
+				boolean found = false;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						found = true;
+						articles.remove(i);
+						System.out.println(id + "번 게시물이 삭제되었습니다.");
+						break;
+					}
+
+					if (found == false) {
+						System.out.println(id + "번 게시물은 존재하지 않습니다.");
+					}
+
+				}
 			}
 
 			else if (command.equals("article write")) {
